a219bc250cc61f24b8efaaa4ac759473079aa626
milanparajuliinfoxit-hue <milanparajuli.infoxit@gmail.com>
2026-07-01 22:38:57 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,7 @@
 const app = require("./src/app");
+const { PORT } = require("./src/config/constant");
 const logger = require("./src/config/winstonLoggerConfig");
 
-const PORT = process.env.PORT || 5000;
-
-console.log("================================");
-console.log("Starting Backend...");
-console.log("PORT:", PORT);
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("================================");
-
-process.on("uncaughtException", (err) => {
-  console.error("UNCAUGHT EXCEPTION");
-  console.error(err);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.error("UNHANDLED REJECTION");
-  console.error(err);
-});
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  logger.info(`Server running on port ${PORT}`);
+  logger.info(`Server is listening at http://localhost:${PORT}`);
 });
