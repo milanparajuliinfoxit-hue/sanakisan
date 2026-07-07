ccd41b4cf7cf3e4e59ff8fc0171a7cac10261756
sachinkharel444 <ukharel111@gmail.com>
2026-07-07 13:50:34 +0545

    Made leadership section dynamic with committee types and positions

--- backend/models/TeamMember.js
+++ backend/models/TeamMember.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database.js');
 const User = require('./User.js');
+const CommitteeType = require('./CommitteeType.js');
+const CommitteePosition = require('./CommitteePosition.js');
 
 const TeamMember = sequelize.define('TeamMember', {
   id: {
@@ -23,11 +25,21 @@ const TeamMember = sequelize.define('TeamMember', {
   },
   type: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
+  },
+  committee_type_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: { model: CommitteeType, key: 'id' },
+  },
+  committee_position_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: { model: CommitteePosition, key: 'id' },
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: true,   
+    allowNull: true,
   },
   contact: {
     type: DataTypes.STRING,
@@ -40,21 +52,20 @@ const TeamMember = sequelize.define('TeamMember', {
   status: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    defaultValue: 1
+    defaultValue: 1,
   },
   created_by: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    references: {
-      model: User,
-      key: 'id'
-    }
-  }
+    references: { model: User, key: 'id' },
+  },
 }, {
   tableName: 'team_members',
   timestamps: true,
 });
 
 TeamMember.belongsTo(User, { foreignKey: 'created_by' });
+TeamMember.belongsTo(CommitteeType, { foreignKey: 'committee_type_id', as: 'committeeType' });
+TeamMember.belongsTo(CommitteePosition, { foreignKey: 'committee_position_id', as: 'committeePosition' });
 
 module.exports = TeamMember;
